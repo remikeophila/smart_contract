61c27e2a05484f2d79a2f80c9f122443c84fed33
alemoigne <adrien.lemoigne@docusign.com>
2018-02-06 00:18:57 +0100

    add mock and jwt

---
+++ nodejs-server-server/controllers/POSTContractService.js
@@ -0,0 +1,13 @@
+'use strict';
+
+exports.createContract = function(args, res, next) {
+  /**
+   * Create contract
+   * Create Contracts
+   *
+   * new Contract Contract Create Contract object 
+   * no response value expected for this operation
+   **/
+  res.end();
+}
+
